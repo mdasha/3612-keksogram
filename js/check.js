bd79b60912d15dd6f39f8ed4764c0bff1bff6416
Daria Slovetskikh <mdasha1979@yandex.ru>
2015-11-15 15:18:30 +0300

    Модуль 1. Задание 2
    Исправлено, согласно замечаниям.

--- js/check.js
+++ js/check.js
@@ -1,28 +1,28 @@
 function getMessage(a, b) {
-	var Message;
-	if (a==true)   {
-		var Message = "Переданное GIF-изображение анимировано и содержит " + b + " кадров";
+	var message;
+	if (a === true)   {
+		var message = "Переданное GIF-изображение анимировано и содержит " + b + " кадров";
 		}
-	else if (a==false) {
-		Message = "Переданное GIF-изображение не анимировано";
+	else if (a === false) {
+		message = "Переданное GIF-изображение не анимировано";
 		}
-	else if (typeof a=="number") {
-		Message = "Переданное SVG-изображение содержит "+ a +" объектов и "+ b*4 +" аттрибутов";
+	else if (typeof a === "number") {
+		message = "Переданное SVG-изображение содержит " + a + " объектов и " + b*4 + " аттрибутов";
 	}
-	else if (((typeof a == "object") && (a instanceof Array)) && ((typeof b == "object") && (b instanceof Array))) {
-		var square=0;
-		for (var i=0; i<a.length; i++ ) {
+	else if (a instanceof Array && b instanceof Array) {
+		var square = 0;
+		for (var i = 0; i < a.length; i++ ) {
 		square += a[i]*b[i];
 		}
-		Message = "Общая площадь артефактов сжатия: " + square +" пикселей";
+		message = "Общая площадь артефактов сжатия: " + square +" пикселей";
 	}
-	else if ((typeof a == "object") && (a instanceof Array)) {
-		var sum=0;
-		for (var i=0; i<a.length; i++ ) {
+	else if (a instanceof Array) {
+		var sum = 0;
+		for (var i = 0; i < a.length; i++ ) {
 		sum += a[i];
 		}
-		Message = "Количество красных точек во всех строчках изображения: "+ sum;
+		message = "Количество красных точек во всех строчках изображения: " + sum;
 	}
-	return Message;
+	return message;
 }
 
